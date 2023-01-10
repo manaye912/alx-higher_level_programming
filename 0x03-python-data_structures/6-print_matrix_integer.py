dbe4c4738a4ef691b8130d3e27fa7e53e910999e
manaye912 <manayelingerew@gmail.com>
2023-01-09 17:50:00 -0800

    matrix draw

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in range(len(row)):
-            print("{:d}".format(row[col]),
-                    end=' ' if col < len(row) - 1 else '')
-            print()
+     for raw in range(len(matrix)):
+         for col in range(len(matrix[i])):
+             print("{:d}".format(matrix[raw][col]), end="")
+             if col != (len(matrix[raw]) - 1):
+                 print(" ", end="")
+                 print("")
