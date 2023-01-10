7060bf6b2c2e17180ddcf2828445d8bf39ddd7f2
manaye912 <manayelingerew@gmail.com>
2023-01-10 04:15:54 -0800

    add infinit numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == '__main__':
 
     total = 0
-    for number in range((sys.argv) - 1):
-        total += int(syt.argv[number + 1])
+    for x in range(len(sys.argv) - 1):
+        total += int(sys.argv[x + 1])
         print("{}".format(total))
