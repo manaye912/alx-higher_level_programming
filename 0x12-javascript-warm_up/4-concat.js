c997fbb6559488b2827f39486835bd414c69fe7d
*manaye* <manayelingerew@gmail.com>
2023-04-11 09:32:49 +0300

    javascript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-if (myArgs[0] && myArgs[1]) {
-  console.log(`${myArgs[0]}` + ' is ' + `${myArgs[1]}`);
-} else if (myArgs[0] && !myArgs[1]) {
-  console.log(`${myArgs[0]}` + ' is ' + 'undefined');
-} else {
-  console.log('undefined is undefined');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
