9a705c611228fba4c5531f5c1eebd60c0bbc8c00
*manaye* <manayelingerew@gmail.com>
2023-04-30 18:29:49 +0300

    write script fetches ...

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""script for testing status of web pages
-"""
-if __name__ == "__main__":
-    import urllib.request
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        bytes_content = response.read()
-        content = bytes_content.decode('utf-8')
-        print_str = '''Body response:
-\t- type: {}
-\t- content: {}
-\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
-        print(print_str)
+'''
+Script that fetches https://intranet.hbtn.io/status
+'''
+from urllib import request
+
+url = 'https://intranet.hbtn.io/status'
+with request.urlopen(url) as response:
+    the_page = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
